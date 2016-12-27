7b415b6237b91de0eb8c4f93ff0cfde7542295e7
manujua82 <manujua@gmail.com>
2016-12-27 12:39:48 -0500

    Adding Images To The Recording Buttons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,10 +33,17 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(_ sender: Any) {
         print("Record button was pressed")
         recordingLabel.text = "Recording in progress"
+        
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
    
     @IBAction func stopRecording(_ sender: Any) {
         print("Stop recording button was pressed")
+        recordingLabel.text = "Tap to Record"
+        
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
     }
 }
 
